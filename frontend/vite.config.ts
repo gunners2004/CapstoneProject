a9a2d7031504ed341867a1ae1e25a2e38c8c42bd
andrew <88shev@ukr.net>
2026-01-08 15:46:23 +0100

    add frontend proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+import { defineConfig } from 'vite'
+
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        // ... Deine Plugins
+    ],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                // optional: changeOrigin: true
+            }
+        }
+    }
 })
